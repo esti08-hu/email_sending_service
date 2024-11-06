c2268d0693b8770a65eb9c50fbcd0581787cf2d2
esti08-hu <estioame@gmail.com>
2024-11-06 15:58:25 +0300

    created welcome and reset-password endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { BullModule } from '@nestjs/bull';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EMAIL_QUEUE } from './constants';
+import { EMAIL_QUEUE } from './constant';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EmailProcessor } from './email.processor';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     BullModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -20,8 +25,22 @@ import { EMAIL_QUEUE } from './constants';
     BullModule.registerQueue({
       name: EMAIL_QUEUE,
     }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_SERVICE,
+        port: Number(process.env.EMAIL_PORT),
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+      },
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EmailProcessor],
 })
 export class AppModule {}
